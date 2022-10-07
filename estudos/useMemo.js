85a5b427cd9a0a30ea4be93700b762a2dfe497fd
Roger-Franco <rogerfrancogalt@gmail.com>
2022-10-06 21:28:47 -0400

    useRef

--- estudos/useMemo.js
+++ estudos/useMemo.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import prop from 'prop-types';
 import './App.css';
-import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
 // useMemo não propaga a renderização do componente Pai para o Filho
 
-const Post = ({ post, handleClick }) => {
+const Post = ({ post }) => {
   console.log('Filho renderizou!');
   return (
     <div key={post.id} className="post">
-      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
+      <h1>{post.title}</h1>
       <p>{post.body}</p>
     </div>
   );
@@ -20,42 +20,25 @@ Post.propTypes = {
     title: prop.string,
     body: prop.string,
   }),
-  handleClick: prop.func,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
-  const input = useRef(null);
-  const contador = useRef(0);
-
   console.log('Pai renderizou!');
 
   useEffect(() => {
-    input.current.focus();
-    console.log(input.current);
-  }, [value]);
-
-  useEffect(() => {
-    contador.current++;
-  });
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
 
   return (
     <div className="App">
-      <h6>Renderizou: {contador.current}X</h6>
       <p>
         <input
-          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -65,9 +48,7 @@ function App() {
       {useMemo(() => {
         return (
           posts.length > 0 &&
-          posts.map((post) => (
-            <Post handleClick={handleClick} key={post.id} post={post} />
-          ))
+          posts.map((post) => <Post key={post.id} post={post} />)
         );
       }, [posts])}
       {posts.length <= 0 && <p>Ainda não há posts</p>}
